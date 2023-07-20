e56e985c9c18b9097460674feeba39e37853321a
TMRGZ <miguelrg5605@outlook.es>
2023-07-20 20:01:53 +0200

    FEATURE: Added Image domain model

---
+++ src/main/java/com/rviewer/skeletons/domain/model/Image.java
@@ -0,0 +1,27 @@
+package com.rviewer.skeletons.domain.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+public class Image {
+
+    private UUID id;
+
+    private String name;
+
+    private String url;
+
+    private OffsetDateTime creationDate;
+
+    private BigDecimal weight;
+
+    private BigDecimal gridPosition;
+
+
+}
