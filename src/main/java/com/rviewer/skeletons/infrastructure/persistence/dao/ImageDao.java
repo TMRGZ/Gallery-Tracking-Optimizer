f0657eda13fec0230101a35326d671ee4448eaa0
TMRGZ <miguelrg5605@outlook.es>
2023-07-20 20:22:52 +0200

    FEATURE: Added image dao document

---
+++ src/main/java/com/rviewer/skeletons/infrastructure/persistence/dao/ImageDao.java
@@ -0,0 +1,23 @@
+package com.rviewer.skeletons.infrastructure.persistence.dao;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Document
+public class ImageDao {
+
+    private UUID id;
+
+    private String name;
+
+    private String url;
+
+    private OffsetDateTime creationDate;
+
+}
