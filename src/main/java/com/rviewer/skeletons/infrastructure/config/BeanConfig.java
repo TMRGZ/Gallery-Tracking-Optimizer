2659e9597634f7fe917942f6010f329762572cb6
TMRGZ <miguelrg5605@outlook.es>
2023-07-21 21:33:24 +0200

    FEATURE: Added debug service bean

--- src/main/java/com/rviewer/skeletons/infrastructure/config/BeanConfig.java
+++ src/main/java/com/rviewer/skeletons/infrastructure/config/BeanConfig.java
@@ -1,8 +1,11 @@
 package com.rviewer.skeletons.infrastructure.config;
 
 import com.rviewer.skeletons.domain.repository.ImageRepository;
+import com.rviewer.skeletons.domain.service.DebugService;
 import com.rviewer.skeletons.domain.service.ImageService;
+import com.rviewer.skeletons.domain.service.impl.DebugServiceImpl;
 import com.rviewer.skeletons.domain.service.impl.ImageServiceImpl;
+import com.rviewer.skeletons.infrastructure.service.DatasetService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,5 +15,10 @@ public class BeanConfig {
     @Bean
     public ImageService imageService(ImageRepository imageRepository) {
         return new ImageServiceImpl(imageRepository);
+    }
+
+    @Bean
+    public DebugService debugService(ImageRepository imageRepository, DatasetService datasetService) {
+        return new DebugServiceImpl(imageRepository, datasetService);
     }
 }
