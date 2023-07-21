7a17585ef4ccd629105bfc3b4c12f7f2e8bb962a
TMRGZ <miguelrg5605@outlook.es>
2023-07-21 21:34:08 +0200

    FEATURE: Created debug api controller

---
+++ src/main/java/com/rviewer/skeletons/infrastructure/controller/DebugApiController.java
@@ -0,0 +1,25 @@
+package com.rviewer.skeletons.infrastructure.controller;
+
+import com.rviewer.skeletons.application.service.DebugApplicationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class DebugApiController implements DebugApi {
+
+    @Autowired
+    private DebugApplicationService debugApplicationService;
+
+    @Override
+    public Mono<ResponseEntity<Void>> deleteData(ServerWebExchange exchange) {
+        return debugApplicationService.deleteData();
+    }
+
+    @Override
+    public Mono<ResponseEntity<Void>> importData(ServerWebExchange exchange) {
+        return debugApplicationService.importData();
+    }
+}
