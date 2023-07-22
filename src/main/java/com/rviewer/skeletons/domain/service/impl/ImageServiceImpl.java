dbc8fb3c55da32cc99c6f4905350fdcf722311fc
TMRGZ <miguelrg5605@outlook.es>
2023-07-22 22:20:11 +0200

    FEATURE: Made use of sorter factory

--- src/main/java/com/rviewer/skeletons/domain/service/impl/ImageServiceImpl.java
+++ src/main/java/com/rviewer/skeletons/domain/service/impl/ImageServiceImpl.java
@@ -3,6 +3,7 @@ package com.rviewer.skeletons.domain.service.impl;
 import com.rviewer.skeletons.domain.model.Image;
 import com.rviewer.skeletons.domain.repository.ImageRepository;
 import com.rviewer.skeletons.domain.service.ImageService;
+import com.rviewer.skeletons.domain.sorter.factory.SorterFactory;
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Flux;
 
@@ -11,9 +12,12 @@ public class ImageServiceImpl implements ImageService {
 
     private final ImageRepository imageRepository;
 
+    private final SorterFactory sorterFactory;
+
     @Override
     public Flux<Image> getImages() {
-        return imageRepository.findAll();
+        return imageRepository.findAll()
+                .sort(sorterFactory.getSorter(""));
     }
 
     @Override
