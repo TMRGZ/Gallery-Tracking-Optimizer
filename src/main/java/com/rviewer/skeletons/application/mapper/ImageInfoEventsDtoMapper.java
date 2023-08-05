76bd458f9cbbc8d8f4238e96ede089ed4c8afa79
Miguel Ángel <MiguelRG5605@Outlook.es>
2023-08-06 01:37:47 +0200

    FEATURE: Removed deadcode

--- src/main/java/com/rviewer/skeletons/application/mapper/ImageInfoEventsDtoMapper.java
+++ src/main/java/com/rviewer/skeletons/application/mapper/ImageInfoEventsDtoMapper.java
@@ -7,8 +7,6 @@ import org.mapstruct.Mapper;
 @Mapper(componentModel = "spring")
 public interface ImageInfoEventsDtoMapper {
 
-    ImageInfoEvents map(ImageInfoEventsDto imageInfoEventsDto);
-
     ImageInfoEventsDto map(ImageInfoEvents imageInfoEvents);
 
 }
--- src/main/java/com/rviewer/skeletons/domain/model/enums/EventTypeEnum.java
+++ src/main/java/com/rviewer/skeletons/domain/model/enums/EventTypeEnum.java
@@ -1,20 +1,5 @@
 package com.rviewer.skeletons.domain.model.enums;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-import java.util.Arrays;
-
-@Getter
-@RequiredArgsConstructor
 public enum EventTypeEnum {
-    VIEW("VIEW"), CLICK("CLICK");
-
-    private final String value;
-
-    public static EventTypeEnum fromValue(String value) {
-        return Arrays.stream(values())
-                .filter(eventTypeEnum -> eventTypeEnum.value.equalsIgnoreCase(value))
-                .findAny().orElseThrow();
-    }
+    VIEW, CLICK
 }
